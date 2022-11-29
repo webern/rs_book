f76954906e0e14263bd2925326e7b51411be271c
Matthew James Briggs <matthew.james.briggs@gmail.com>
2022-11-28 17:27:32 -0800

    chapter_6

--- chapter_6/src/main.rs
+++ chapter_6/src/main.rs
@@ -43,16 +43,138 @@ mod enums_with_and_without_data {
         UnnamedStruct { x: u64, y: u64 },
     }
 
+    // You can implement functions on enums!
+    impl DataEnum {
+        pub fn explode(&self) {
+            match self {
+                DataEnum::Empty => todo!(),
+                DataEnum::Point(Point { x: 0, .. }) => todo!(),
+                DataEnum::UnnamedStruct { x: 0, .. } => todo!(),
+                _ => todo!(),
+            }
+        }
+    }
+
     fn example() {
         let value_1 = DataEnum::Empty;
         let value_2 = DataEnum::Point(Point { x: 0, y: 1 });
         let value_3 = DataEnum::UnnamedStruct { x: 0, y: 1 };
 
-        match value_3 {
-            DataEnum::Empty => todo!(),
-            DataEnum::Point(Point { x: 0, .. }) => todo!(),
-            DataEnum::UnnamedStruct { x: 0, .. } => todo!(),
-            _ => todo!(),
-        }
+        value_3.explode();
     }
 }
+
+/// The Option Enum, p. 101. Does away with `null`.
+fn option_unwraps() {
+    let null: Option<u8> = None;
+    let not_null: Option<u8> = Some(0u8);
+
+    // getting the value from an option.
+    // This is panic!
+    let x = null.unwrap();
+
+    // This will give the default of the internal type if the option is `None` (in this case 0)
+    let x = null.unwrap_or_default();
+
+    // This will give the value of our choice if the option is `None`
+    let x = null.unwrap_or(255);
+
+    // This will call the provided lambda function if the option is `None`.
+    let x = null.unwrap_or_else(|| {
+        // do some expensive calculation to come up with the default
+        42
+    });
+}
+
+enum Letter {
+    A,
+    B,
+    C,
+}
+
+/// Exhaustive Matches p. 104
+///
+/// Note: The `match` operator is like `case` in other languages.
+///
+fn exhaustive_matches() {
+    // We must cover all possible code branches when using the match operator
+
+    // Won't compile!
+    // let letter = Letter::C;
+    // match letter {
+    //     Letters::A => println!("A"),
+    //     Letters::B => println!("B"),
+    //     // oh, no, we haven't covered all cases
+    // }
+
+    // This is Ok because we cover all cases:
+    let letter = Letter::B;
+    match letter {
+        Letter::A => println!("A"),
+        Letter::B => println!("B"),
+        Letter::C => println!("C"),
+    }
+
+    // This is also Ok because we use the "catch all" case:
+    match letter {
+        Letter::A => println!("A"),
+        _ => println!("something else"),
+    }
+
+    // Covering all conditions applies to any type. We can match on a &str:
+    let some_str = "hello";
+
+    // This won't compile!
+    // match some_str {
+    //     "hello" => println!("hello world"),
+    // }
+
+    // This uses the "catch all" so it's fine:
+    match some_str {
+        "hello" => println!("-------------- HELLO"),
+        _ => println!("unknown string"),
+    }
+
+    // Here's some very interesting syntax. You can introduce a variable in the match arms:
+    match some_str {
+        "hello" => println!("-------------- HELLO"),
+        new_str_variable => println!("{}", new_str_variable), // prints the value of some_str
+    }
+
+    // Here's a subtle mistake that can be very confusing. It's a bug because I forgot to use
+    // `Letters::` in front of the `A`. I'm writing `A` instead of `Letter::A` and the compiler
+    // thinks it is a variable named `A`.
+    //
+    // This will **ALWAYS** print "A"/
+    // Fortunately the compiler warns me that the _ => case is unreachable.
+    // match letter {
+    //     A => println!("A"),
+    //     _ => println!("something else"),
+    // }
+}
+
+enum Value {
+    I(u64),
+    S(&'static str),
+}
+
+/// Patterns that bind to values. p. 105
+///
+/// Pattern matching (i.e. using the `match` operator) can get pretty deep syntax-wise. You can
+/// match a certain variant only for certain values, for example:
+fn patterns_that_bind_to_values() {
+    let value = Value::S("hello");
+    match value {
+        Value::S("hello") => println!("I know the value was 'hello'"),
+        Value::I(42) => println!("I know the value was 42)"),
+        _ => println!("I need the catch-all because I haven't covered all values"),
+    }
+
+    // We can also assign whatever is "in" the variant to a variable:
+    match value {
+        Value::I(i) => println!("{}", i),
+        Value::S(s) => println!("{}", s),
+    }
+}
+
+// If let. These two blocks of code are equivalent
